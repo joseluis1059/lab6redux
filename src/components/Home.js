27ab27ffe05c0ebb9b0f79618baa44182f1fcd3a
JoseLuisDavid19 <Joseluisyoda@gmail.com>
2019-06-20 08:46:52 -0600

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -15,21 +15,6 @@ import {
 import React, {Component} from 'react';
 import ACTIONS from "../modules/action";
 import { connect } from "react-redux";
-import { withRouter } from 'react-router-dom'
-
-const styles = theme => ({
-    root: {
-      flexGrow: 1,
-      maxWidth: 752
-    },
-    demo: {
-      backgroundColor: theme.palette.background.paper
-    },
-    title: {
-      margin: `${theme.spacing.unit * 4}px 0 ${theme.spacing.unit * 2}px`
-    }
-  });
-
 
 const mapStateToProps = state => ({
     homeTitles: state.homeTitle
@@ -72,4 +57,4 @@ class Home extends Component{
 export default connect(
     mapStateToProps,
     mapDispatchToProps
-  )(withStyles(styles)(Home));
+  )(Home);
